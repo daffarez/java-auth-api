69aeaec3d9d1c2746dfb3a27baaa938e7b626f1b
Daffarez Elguska <daffaelguska@gmail.com>
2026-02-13 16:31:21 +0700

    Initial commit: setup project and user model

---
+++ src/main/java/com/example/javaauthapi/JavaAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaauthapi/JavaAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
