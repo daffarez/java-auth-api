d38847ba82c4b1b6e35b3c0ce4f09d51ff87f2a0
Daffarez Elguska <daffaelguska@gmail.com>
2026-02-13 17:13:17 +0700

    add auth controller, register endpoint

---
+++ src/main/java/com/example/javaauthapi/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.example.javaauthapi.controller;
+
+import com.example.javaauthapi.model.User;
+import com.example.javaauthapi.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        try {
+            User registeredUser = authService.register(user);
+            return ResponseEntity.status(201).body(registeredUser.getUsername() + " successfully registered.");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
